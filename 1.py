3260fe0c50975bf31c3500a2e7ef6f8e4bb4d953
RikhardHonkanen <75729661+RikhardHonkanen@users.noreply.github.com>
2023-12-02 14:17:13 +0100

    we got there day 1

--- 1.py
+++ 1.py
@@ -9,17 +9,36 @@ numbers_as_words = {
     "six":      "6",
     "seven":    "7",
     "eight":    "8",
-    "nine":     "9",
+    "nine":     "9"
 }
 
-def find_first_number(cal):
-    #we might fix this one day (don't count on it)
-    pass
+word_index_map = []
 
-def find_first_number_as_string(cal):
+def find_lowest_index_character(indexes):
+    lowest = -1
+    answer = 0
+    for pair in indexes:
+        for key in pair:
+            if ((pair[key] != -1 and pair[key] < lowest) or lowest == -1 ):
+                lowest = pair[key]
+                answer = key
+    return answer  
+    
+
+def find_first_int_index(cal):
+    for idx, char in enumerate(cal):
+            if (char.isdigit()):
+                return { char: idx }
+
+def find_first_number_as_string_indexes(cal, reverse = False):
+    word_index_map = []
     for key in numbers_as_words.keys():  
-        if key in cal:
-            return numbers_as_words[key]  
+        compare = key if not reverse else reverse_string(key)
+        if compare in cal:
+            word_index_map.append({ numbers_as_words[key]: cal.index(compare) }) 
+        else:
+            word_index_map.append({ numbers_as_words[key]: -1 }) 
+    return word_index_map
 
 def parse_coordinates(calibration_values):
     for jumbled_string in calibration_values:
@@ -28,21 +47,19 @@ def parse_coordinates(calibration_values):
         results.append(str(first_char) + str(second_char))    
 
 def get_first_coordinate(cal):  
-    if (find_first_number_as_string(cal)):
-        return find_first_number_as_string(cal)
-    else:
-        for char in cal:
-            if (char.isdigit()):
-                return char
+    first_int_index = find_first_int_index(cal)
+    word_indexes = find_first_number_as_string_indexes(cal)
+    word_indexes.append(first_int_index)
+    coordinate = find_lowest_index_character(word_indexes)
+    return coordinate
 
 def get_last_coordinate(cal):
     reversed_cal = reverse_string(cal)    
-    if (find_first_number_as_string(reversed_cal)):
-        return find_first_number_as_string(reversed_cal)
-    else:
-        for char in cal:
-            if (char.isdigit()):
-                return char
+    first_int_index = find_first_int_index(reversed_cal)
+    word_indexes = find_first_number_as_string_indexes(reversed_cal, True)
+    word_indexes.append(first_int_index)
+    coordinate = find_lowest_index_character(word_indexes)
+    return coordinate
 
 def parse_file(path):
 	with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), path), "r") as f:
@@ -58,8 +75,11 @@ def add_all_coordinates(results):
 def reverse_string(input):
   return input[::-1]
 
+# test = find_first_number_as_string_indexes("twobrrone")
+# test = find_first_int_index("t9wo821brrone")
+# test = find_lowest_index_character([{'1': 6}, {'2': 0}, {'3': -1}, {'4': -1}, {'5': -1}, {'6': -1}, {'7': -1}, {'8': -1}, {'9': -1}])
 results = []
-calibration_values = parse_file('input.txt')
+calibration_values = parse_file('1.txt')
 single_coordinate = parse_coordinates(calibration_values)
 answer = add_all_coordinates(results)
 print(answer)
