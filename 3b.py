3233a38f9132062266e30b4441a50db472590402
RikhardHonkanen <75729661+RikhardHonkanen@users.noreply.github.com>
2023-12-03 15:03:48 +0100

    day 3 done

--- 3b.py
+++ 3b.py
@@ -11,48 +11,37 @@ import re
 symbol_indexes_map = {}
 # hardcoded input rows, because lazy
 num_rows = 140
-# num_rows = 10
 
 def store_symbol_indexes(schematic):
     row_length = len(schematic[0])
     for idx, line in enumerate(schematic):
         indexes = []
         for cidx, char in enumerate(line):
-            if(not char.isdigit() and char != '.'):
+            if(char == '*'):
                 indexes.append(cidx)
         symbol_indexes_map[idx] = indexes
     return row_length
         
 def compare_symbol_number_locations(idx, numbers_with_locations):
-    eligible_parts = []
     for number_location in numbers_with_locations:
         compare_to_adjacent_rows(idx, number_location)
     
 def compare_to_adjacent_rows(idx, number_location):
     n = list(number_location.keys())[0]
-    print(f'In: {n}')
     bounds = list(number_location.values())[0]
     
-    # note to self: risk of off by one errors here
     min_bound = bounds[0] - 1 if bounds[0] - 1 >= 0 else bounds[0]
     max_bound = bounds[1] if bounds[1] <= row_length + 1 else bounds[1] - 1
     top_row_to_check = idx - 1 if idx - 1 >= 0 else idx
     bottom_row_to_check = idx + 2 if idx + 2 <= num_rows else idx + 1
     
-    print(f'Rows: {num_rows}')
-    print(f'Top row: {top_row_to_check}')
-    print(f'Bottom row: {bottom_row_to_check}')
-    print(f'Left index: {min_bound}')
-    print(f'Right index: {max_bound}')
-    
     for vindex in range(top_row_to_check, bottom_row_to_check):
-        print(f'Checking row: {vindex}')
-        print(f'Symbolindexes on row: {symbol_indexes_map[vindex]}')
         for hindex in range(min_bound, max_bound):
-            print(f'Symbolindexes on row: {symbol_indexes_map[vindex]}')
             if(int(hindex) in symbol_indexes_map[vindex]):
-                print(f'Out: {n}')
-                results.append(n)
+                if((hindex, vindex) in results):
+                    results[(hindex, vindex)] += [n]
+                else:
+                    results[(hindex, vindex)] = [n]
                 break
         
 def extract_number_locations(line):
@@ -70,20 +59,22 @@ def parse_file(path):
 		parsed_input = f.read().split("\n")
 	return parsed_input
 
-def add_all_results(results):
+def calculate_answer(results):
     sum = 0
-    for res in results:
-        sum += int(res)
+    values = results.values()
+    for val in values:
+        if(len(val) == 2):
+            sum += int(val[0]) * int(val[1])
     return sum
 
-results = []
+results = {}
 schematic = parse_file('3.txt')
 row_length = store_symbol_indexes(schematic)
 populate_results(schematic)
-answer = add_all_results(results)
-print(f'Results: {results}')
-# test expect: 4361
+answer = calculate_answer(results)
+# print(f'Results: {results}')
+# test expect: 467835
 print(answer)
 
-### I confused myself with min and max bounds and had to spend a lot of time debugging
-### The approach is probably not the most elegant, but it gets part 1 done
+### I was able to adapt my solution pretty well for part 2
+### still took me forever to get my data neat, probably due to fatigue
