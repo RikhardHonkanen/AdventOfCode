78046493e84da8cb4c4e78e08b04750929dc7365
RikhardHonkanen <75729661+RikhardHonkanen@users.noreply.github.com>
2023-12-06 16:54:14 +0100

    day 5 still borked

--- 5.py
+++ 5.py
@@ -48,34 +48,49 @@ def populate_maps(clean_data):
         else:
             maps[current_key] = [number_list]
             
-def walk_the_maps(seed, lookup_table):
+def walk_the_maps(seed):
     print_debug(f"Seed: {seed}")
     current = seed
-    path = []
     for rows in maps:
         print_debug(f"Current data: {rows}: {maps[rows]}")
-        for idx, r in enumerate(maps[rows]):
-            if (idx == 2):
-                if (current in lookup_table):
-                    print(lookup_table)
-                    print_debug(f"Path explored, destination: {lookup_table[current]}")
-                    return -1, []
-            
+        for r in maps[rows]:
             source_min = int(r[1]) 
             source_max = int(r[1]) + int(r[2]) - 1
             print_debug(f"Source min, source max: {source_min} - {source_max}")
             if (int(current) >= source_min and source_max >= int(current)):
                 current = int(r[0]) + (int(current)) - source_min
-                path.append(current)
                 print_debug(f"Match! New current: {current}")
                 break
             print_debug(f"No match. Current is still: {current}")
-        # If no match, record path and go to next map
-        path.append(current)
         
     print_debug(f"Destination: {current}")
     print_debug(f"------------------------------------")
-    return current, path
+    return current
+
+def maps_the_walk(seed_range, map_ranges):
+    # (StartA <= EndB)  and  (EndA >= StartB)
+    print(seed_range)
+    print(map_ranges)
+    current_range = seed_range
+    for i in range(0, len(map_ranges)):
+        for pair in map_ranges[i]:
+            print(pair)
+            if (seed_range[0] <= pair[0][1] and seed_range[1] >= pair[0][0]):
+                print('No overlap')
+            else:
+                print(pair[1])
+    exit()
+
+def convert_maps_to_ranges():
+    all_map_ranges = []
+    for m in maps:
+        single_map_ranges = []        
+        for i in maps[m]:
+            from_range = (int(i[1]), int(i[1]) + int(i[2]))
+            to_range = (int(i[0]), int(i[0]) + int(i[2]))
+            single_map_ranges.append((from_range, to_range))
+        all_map_ranges.append(single_map_ranges)
+    return all_map_ranges
 
 def get_ranges(seeds):
     ranges = []
@@ -88,21 +103,16 @@ def get_ranges(seeds):
 def find_nearest_location(seed_ranges = False):
     lowest = -1
     if seed_ranges:
-        ranges = get_ranges(seeds)        
+        map_ranges = convert_maps_to_ranges()    
+        ranges = get_ranges(seeds)
+        # TODO: try comparing entire ranges at once instead        
         for r in ranges:
-            lookup_table = {}
-            for seed in range(r[0], r[1]):
-                destination, path = walk_the_maps(seed, lookup_table)
-                if (destination == -1):
-                    continue
-                if (path):
-                    lookup_table[path[1]] = destination
-                    # print(lookup_table)
-                if (destination < lowest or lowest == -1):
-                    lowest = destination             
+            destination = maps_the_walk(r, map_ranges)
+            if (destination < lowest or lowest == -1):
+                lowest = destination             
     else:
         for seed in seeds:
-            destination = walk_the_maps(seed, {})
+            destination = walk_the_maps(seed)
             if (destination < lowest or lowest == -1):
                 lowest = destination 
         
@@ -135,8 +145,8 @@ def part_two(input, _debug = False):
 if __name__ == "__main__":
     P1TEST, P2TEST = 35, 46
     test_input, input = parse_file("5test.txt"), parse_file("5.txt")
-    # print(f"Part 1 Test: {part_one(test_input, False)} (expected {P1TEST})")
-    print(f"Part 2 Test: {part_two(test_input, True)} (expected {P2TEST})")
+    print(f"Part 1 Test: {part_one(test_input, False)} (expected {P1TEST})")
+    print(f"Part 2 Test: {part_two(test_input, False)} (expected {P2TEST})")
     # print()
     # print(f"Part 1: {part_one(input)}")
     # print(f"Part 2: {part_two(input)}")
@@ -144,3 +154,7 @@ if __name__ == "__main__":
     ### Part one wasn't too bad, mostly tedious with all the parsing
     ### I figured out a solution for the test case for part 2, but it didn't scale to the input data (computer dying)
     ### Tried to implement some kind of lookup table, but it probably made things worse... and broke part 1 in the process
+    
+    
+    ### seed-to-soil map:
+    ### {(98, 100): (50, 52), }
