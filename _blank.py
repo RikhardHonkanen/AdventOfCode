cdbefba2fb1418fb95220cd18023c3342bcadd7a
RikhardHonkanen <rikhard@raket.co>
2024-12-02 08:02:07 +0100

    template file

--- _blank.py
+++ _blank.py
@@ -13,31 +13,19 @@ def parse_file(path):
 		parsed_input = f.read().split('\n')
 	return parsed_input
 
-def print_debug(string):
-    if (debug):
-        print(string)
-
-def part_one(input, _debug = False):
-    if (_debug):
-        global debug
-        debug = True
-    
+def part_one(input):      
     answer = 'Part one'            
     return answer
 
-def part_two(input, _debug = False):
-    if (_debug):
-        global debug
-        debug = True
-        
+def part_two(input):
     answer = 'Part two'            
     return answer
 
 if __name__ == "__main__":
     P1TEST, P2TEST = 0, 0
     test_input, input = parse_file("0test.txt"), parse_file("0.txt")
-    print(f"Part 1 Test: {part_one(test_input, False)} (expected {P1TEST})")
-    # print(f"Part 2 Test: {part_two(test_input, False)} (expected {P2TEST})")
+    print(f"Part 1 Test: {part_one(test_input)} (expected {P1TEST})")
+    # print(f"Part 2 Test: {part_two(test_input)} (expected {P2TEST})")
     # print()
     # print(f"Part 1: {part_one(input)}")
     # print(f"Part 2: {part_two(input)}")
